a0d0c19f2a3743acb7db726acb4ba997f42d53bc
Sarah Lessner <sarahlessner@Sarahs-MacBook-Pro.local>
2017-07-31 00:10:43 -0700

    added some new friends to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,19 +15,43 @@ var friendsArray = [
 		survey: [2, 5, 1, 2, 4, 1, 5, 1, 3, 5]
 	},
 	{
-		name: 'Beatrice Jones',
-		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRugj0V1KwxBbN905ksAaiI7uyi5pxhQPyQXI7cBenyl8TNTE56',
+		name: 'Watermelon Woman',
+		photo: 'http://i.huffpost.com/gadgets/slideshows/397024/slide_397024_4882686_free.jpg',
 		survey: [4, 1, 4, 3, 2, 3, 1, 2, 3, 2]
 	},
 	{
-		name: 'Connor Johnson',
-		photo: 'http://www.vizit.com/wp-content/uploads/2012/10/stock-photo-14787161-confusion-young-man-scratching-his-head-on-white-300x234.jpg',
+		name: 'Bill Randall',
+		photo: 'https://img.buzzfeed.com/buzzfeed-static/static/2014-05/enhanced/webdr02/13/17/original-21277-1400016575-10.jpg?downsize=715:*&output-format=auto&output-quality=auto',
 		survey: [3, 4, 3, 4, 1, 2, 3, 5, 3, 4]
+	},
+	{
+		name: 'Tanya Lawrence',
+		photo: 'http://i.imgur.com/bw2Kdxu.jpg',
+		survey: [2, 4, 3, 2, 3, 5, 1, 5, 1, 4]
+
 	},
 	{
 		name: 'Griz',
 		photo: 'https://tinyurl.com/yba9d3sl',
 		survey: [5, 4, 4, 4, 1, 1, 2, 5, 3, 3]
+
+	},
+	{
+		name: 'Pineapple Head',
+		photo: 'https://s-media-cache-ak0.pinimg.com/236x/77/05/b3/7705b336f6352c410cd92b5ae0841422--pineapple-express-skyrim.jpg',
+		survey: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+	},
+	{
+		name: 'Laura Sanders',
+		photo: 'http://s3.svet-zdravi.cz/nff/d0l3s_iStock_000015185024Large-2-maly.jpg',
+		survey: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+
+	},
+	{
+		name: 'Gregory Young',
+		photo: 'https://s-media-cache-ak0.pinimg.com/originals/a9/cf/a5/a9cfa5874bfe6029e051de4e096ffa90.jpg',
+		survey: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 
 	},
 
